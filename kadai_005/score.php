8d1ca388b23293fca411e5e6b7b3c039f3e0a304
aiatto <aiatto@outlook.jp>
2025-05-18 11:28:06 +0900

    Update score.php

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -2,31 +2,34 @@
 <html lang="ja">
 
 <head>
-   <meta charset="UTF-8">
-   <title>PHP基礎編</title>
+  <meta charset="utf-8">
+  <title>テストの平均点を計算しよう</title>
 </head>
 
 <body>
-   <p>
-<?php
-$numbers = [80, 60, 55, 40, 100, 25, 80, 95, 30, 60];
+  <p>
+    <?php
+    $score1 = 80;
+    $score2 = 60;
+    $score3 = 55;
+    $score4 = 40;
+    $score5 = 100;
+    $score6 = 25;
+    $score7 = 80;
+    $score8 = 95;
+    $score9 = 30;
+    $score10 = 60;
 
-// 配列の要素の合計を求める
-$sum = array_sum($numbers);
+    // 合計点
+    $total = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
 
-// 配列の要素数を求める
-$count = count($numbers);
+    // 平均点
+    $average = $total / 10;
 
-// 平均値を計算する
-$average = $sum / $count;
-
-// 平均値を表示する
-echo "平均値: " . $average;
-?>
-   </p>
-
-
-    
+    // 平均点を出力
+    echo $average;
+    ?>
+  </p>
 </body>
 
 </html>
