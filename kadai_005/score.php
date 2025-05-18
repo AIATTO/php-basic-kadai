f6d1e40dc9f94b30597b5fc478af8d26db9dc69b
aiatto <aiatto@outlook.jp>
2025-05-18 11:18:57 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>
+<?php
+$numbers = [80, 60, 55, 40, 100, 25, 80, 95, 30, 60];
+
+// 配列の要素の合計を求める
+$sum = array_sum($numbers);
+
+// 配列の要素数を求める
+$count = count($numbers);
+
+// 平均値を計算する
+$average = $sum / $count;
+
+// 平均値を表示する
+echo "平均値: " . $average;
+?>
+   </p>
+
+
+    
+</body>
+
+</html>
